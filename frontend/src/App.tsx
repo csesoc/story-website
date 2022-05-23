494b5afe51c4be7d26189846137b686f117d7cb2
Ryan Shi <ryan.shi42@gmail.com>
2022-05-23 13:40:56 +1000

    started header bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,16 +6,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from './pages/Register';
+import Header from './pages/Header';
 
 const App: React.FC<{}> = () => {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
+    
   );
 }
 

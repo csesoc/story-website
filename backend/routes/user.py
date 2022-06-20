1ded95752d82349273e4cce04c8e24bfe5ce1421
Expressionless Ball Thing (Westley) <lowestley0409@gmail.com>
2022-06-20 14:18:44 +1000

    Made some stub code for the user routes
    Made some stub code for the user routes.

---
+++ backend/routes/user.py
@@ -0,0 +1,128 @@
+from flask import Blueprint, jsonify, request
+from flask_jwt_extended import jwt_required, create_access_token, set_access_cookies, unset_jwt_cookies, verify_jwt_in_request
+
+import re
+
+from common.exceptions import AuthError, RequestError
+from common.database import get_connection
+
+user = Blueprint("user", __name__)
+
+@user.route("/profile", methods=["GET"])
+def get_profile():
+    try:
+        header, data = verify_jwt_in_request()
+
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT email FROM Users WHERE uid = %s", (data['id']))
+        email = cursor.fetchone()[0]
+
+        cursor.execute("SELECT username FROM Users WHERE uid = %s", (data['id']))
+        username = cursor.fetchone()[0]
+
+        return {
+            "email": email,
+            "username": username
+        }
+    except:
+        raise AuthError("Invalid Token")
+
+
+
+@user.route("/user/stats", methods=['GET'])
+def get_stats():
+    token = request.args.get('token')
+    competition = request.args.get('competition')
+
+    # Raise RequestError if competition is not valid ()
+
+    try:
+        header, content = verify_jwt_in_request()
+
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT name FROM Competitions WHERE uid = %s", (content['id']))
+        competition = cursor.fetchone()[0]
+        if competition is "":
+            raise RequestError
+        
+        ## find a way to get the stat infos, they are spread across multiple tables.
+
+        return {
+            "stats": []
+        }
+    except:
+        raise AuthError("Invalid token")
+
+
+@user.route("/user/set_name", methods=['POST'])
+def set_name():
+    json = request.get_json()
+    '''
+    {
+    token: token (in cookies)
+    username: string
+    }
+    '''
+    # if username already in database, raise RequestError.
+    try:
+        verify_jwt_in_request()
+        return {}
+    except:
+        raise AuthError("Invalid token")
+
+@user.route("/user/reset_email/request", methods=["POST"])
+def reset_email_request():
+    data = request.get_json()
+    '''
+    {
+        token: token (in cookies)
+        email: string
+    }
+    '''
+    try:
+        verify_jwt_in_request()
+    except:
+        raise AuthError("Invalid token")
+
+
+    # Check if email refers to an actual email.
+    if not re.match('^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$', data['email']):
+        raise RequestError("email not valid")
+    return {}
+
+
+
+    
+@user.route("/user/reset_email/reset", methods=['POST'])
+def reset_email():
+    json = request.get_json()
+    '''
+    {
+    token: token (in cookie)
+    reset_code: string
+    }
+    '''
+    try:
+        verify_jwt_in_request()
+    except:
+        raise AuthError("Invalid token")
+
+    '''
+    if (json['reset_code'] not match the code in database for user):
+        raise AuthError("The reset code is wrong.")
+    else:
+        reset email.
+    '''
+
+
+
+@user.route("/user/reset_password/request", methods=["POST"])
+def reset_password_request():
+    json = request.get_json()
+    return jsonify({})
+
+

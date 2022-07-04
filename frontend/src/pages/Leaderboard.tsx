6c5deb07efadeffbce5cf32318cf6f9cdce7dddb
Ryan Shi <63448382+ryanshi42@users.noreply.github.com>
2022-07-04 12:59:36 +1000

    Frontend (#55)
    * fix pipeline 21st may
    
    * started header bar
    
    * about page finished, nav finished, footer added, ready for subcom to work
    
    * calendar and home page split up
    
    * database helper functions basically done
    
    * implemented the drop everything in db function

---
+++ frontend/src/pages/Leaderboard.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import { Button, Container } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
+import { BACKEND_URI } from "src/config";
+
+const Leaderboard: React.FC<{}> = () => {
+  const [times, setTimes] = useState(0);
+
+  useEffect(() => {
+    const verifyToken = async () => {
+      const result = await fetch(`${BACKEND_URI}/verify`, )
+    };
+
+    verifyToken();
+  }, []);
+
+  return (
+    <>
+      <Container className="justify-content-center text-center">
+        <p>You have clicked this button {times} times.</p>
+        <Button onClick={() => setTimes(times + 1)}>Leaderboard Page</Button>
+      </Container>
+    </>
+  )
+};
+
+export default Leaderboard;

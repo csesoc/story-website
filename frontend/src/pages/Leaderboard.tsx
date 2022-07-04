e6c4f49c5dc5fa899bf563bee117105fab30bdb1
a-jason-liu21 <a.jason.liu21@gmail.com>
2022-07-04 16:19:20 +1000

    added leaderboard page with test data

--- frontend/src/pages/Leaderboard.tsx
+++ frontend/src/pages/Leaderboard.tsx
@@ -1,11 +1,42 @@
 import React, { useEffect, useState } from "react";
 import { Button, Container } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import styles from "../App.module.css";
+import UserProfile from "../components/leaderboard/UserProfile"
 
 import { BACKEND_URI } from "src/config";
 
+export interface UserDetails {
+  position: number;
+  userName: string;
+  userScore: number;
+  userLink: string;
+}
+
 const Leaderboard: React.FC<{}> = () => {
-  const [times, setTimes] = useState(0);
+
+  const defaultUsers: UserDetails[] = [
+    {
+      position: 1,
+      userName: "csesoc",
+      userScore: 9999,
+      userLink: "https://github.com/csesoc"
+    },
+    {
+      position: 10,
+      userName: "no_github_link",
+      userScore: 999,
+      userLink: ""
+    },
+    {
+      position: 100,
+      userName: "a-jason-liu21",
+      userScore: 99,
+      userLink: "https://github.com/a-jason-liu21"
+    }
+  ]
+
+  const [users, setUsers] = useState(defaultUsers);
 
   useEffect(() => {
     const verifyToken = async () => {
@@ -16,12 +47,23 @@ const Leaderboard: React.FC<{}> = () => {
   }, []);
 
   return (
-    <>
-      <Container className="justify-content-center text-center">
-        <p>You have clicked this button {times} times.</p>
-        <Button onClick={() => setTimes(times + 1)}>Leaderboard Page</Button>
-      </Container>
-    </>
+    <div className={styles.leaderboardPage}>
+      <div>
+        This is the overall leaderboard for the <span className={styles.bold}>CSESoc Unnamed Puzzle Competition 2021</span>,
+        which displays the users with the highest cumulative points achieved during the contest.
+      </div>
+      <br />
+      
+      <div>
+        Points awarded for completing problems - the first to solve a problem receives 100 points, followed by 99 points for
+        the second solver, then 98 and so on down to 1 point for the 100th solver.
+      </div>
+      <br />
+
+      {users.map((user: UserDetails) => {
+        return <UserProfile position={user.position} userName={user.userName} userScore={user.userScore} userLink={user.userLink}/>
+      })}
+    </div>
   )
 };
 

6573b00e3be064713069d52aa8979b8b1b513a8c
Hanyuan Li <hanyuan.work@gmail.com>
2022-08-24 12:28:47 +1000

    feat(auth): add timeout capabilities

--- backend/test/auth/login_test.py
+++ backend/test/auth/login_test.py
@@ -1,4 +1,7 @@
+from datetime import datetime, timedelta
+
 import pytest
+from freezegun import freeze_time
 
 # Import for pytest
 from flask.testing import FlaskClient
@@ -75,6 +78,53 @@ def test_lockout(client):
     })
 
     assert response.status_code == 401
+
+def test_lockout_timing(client):
+    clear_all()
+
+    db_add_user("asdfghjkl@gmail.com", "asdf", "foobar")
+
+    # Incorrect login 3 times
+    for _ in range(3):
+        response = client.post("/auth/login", json={
+            "email": "asdfghjkl@gmail.com",
+            "password": "foobaz"
+        })
+
+        assert response.status_code == 401
+
+    timeout_over = datetime.now() + timedelta(minutes=1, seconds=5)
+
+    # Incorrect login again
+    with freeze_time(timeout_over):
+        response = client.post("/auth/login", json={
+            "email": "asdfghjkl@gmail.com",
+            "password": "foobaz"
+        })
+
+        assert response.status_code == 401
+
+    still_timeout = timeout_over + timedelta(minutes=1)
+
+    # Timeout is now 2 minutes
+    with freeze_time(still_timeout):
+        response = client.post("/auth/login", json={
+            "email": "asdfghjkl@gmail.com",
+            "password": "foobar"
+        })
+
+        assert response.status_code == 401
+
+    second_timeout_over = still_timeout + timedelta(minutes=1, seconds=5)
+
+    # Timeout is now 2 minutes
+    with freeze_time(second_timeout_over):
+        response = client.post("/auth/login", json={
+            "email": "asdfghjkl@gmail.com",
+            "password": "foobar"
+        })
+
+        assert response.status_code == 200
 
 def test_protected_route(client: FlaskClient):
     clear_all()
--- backend/test/fixtures.py
+++ backend/test/fixtures.py
@@ -1,12 +1,20 @@
 import pytest
 
+from test.mock.mock_mail import mailbox
+from test.mock.mock_redis import fake_redis
+
 from app import create_app
 
 @pytest.fixture()
-def app():
-    app = create_app()
-    app.config["TESTING"] = True
+def app(mocker):
+    # Mock mailbox
+    mocker.patch("app.mail", mailbox)
+    mocker.patch("common.plugins.mail", mailbox)
 
+    # Mock redis
+    mocker.patch("common.redis.cache", fake_redis)
+
+    app = create_app({"TESTING": True})
     yield app
 
 @pytest.fixture()
